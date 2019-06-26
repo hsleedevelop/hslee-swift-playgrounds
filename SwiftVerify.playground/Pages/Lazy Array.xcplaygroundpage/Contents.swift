d0aa9db1534b2c68ba4903e7abf5b6c912ce38d3
HS Lee <hyungsuk.lee@iriver.com>
2019-06-26 15:44:26 +0900

    add array test

--- SwiftVerify.playground/Pages/Lazy Array.xcplaygroundpage/Contents.swift
+++ SwiftVerify.playground/Pages/Lazy Array.xcplaygroundpage/Contents.swift
@@ -3,14 +3,27 @@
 import Foundation
 
 let resutls = [1,2,3,4,5].filter {
-    print($0)
+//    print($0)
     return $0 > 3
 }
+
 let resutls2 = [1,2,3,4,5].lazy.filter {
     print($0)
     return $0 > 3
 }
 
-print(Array(resutls2))
+let resutls3 = [1,2,3,4,5].first (where: {
+//    print($0)
+    return $0 > 3
+})
+
+let resutls4 = [1,2,3,4,5].lazy.first (where: {
+//    print($0)
+    return $0 > 3
+})
+
+
+//print(Array(resutls2))
+print(resutls2.first)
 
 //: [Next](@next)
