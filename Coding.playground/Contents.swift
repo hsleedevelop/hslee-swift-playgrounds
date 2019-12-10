8ec8d26553ce89a78784cb348adf27e1554987dd
HS Lee <hs.lee@cardoc.kr>
2019-12-10 12:30:36 +0900

    update source

--- Coding.playground/Contents.swift
+++ Coding.playground/Contents.swift
@@ -5,23 +5,44 @@ import UIKit
 var str = "Hello, playground"
 print(str)
 
-var result = ""
-func fibo(n: Int) -> Int {
-    if n < 2 {
-//        result += "\(n) "
-        return n
-    }
-    
-    let rrr = fibo(n-1) + fibo(n-2)
-    result += "\(rrr) "
-    return rrr
+//var result = ""
+//func fibo(n: Int) -> Int {
+//    if n < 2 {
+////        result += "\(n) "
+//        return n
+//    }
+//
+//    let rrr = fibo(n-1) + fibo(n-2)
+//    result += "\(rrr) "
+//    return rrr
+//}
+//
+//var input = 7
+////for i in   0 ... input {
+//////    let result = fibo(i)
+////    print(result)
+////}
+//
+//fibo(input)
+//print("\(result)\n")
+
+let date1 = Date(timeIntervalSinceNow: -10)
+let date2 = Date(timeIntervalSinceNow: 10)
+
+print(date1)
+print(Date())
+print(date2)
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+    print(date1.timeIntervalSinceNow)
+    print(Date().timeIntervalSinceNow)
+    print(date2.timeIntervalSinceNow)    
 }
 
-var input = 7
-//for i in   0 ... input {
-////    let result = fibo(i)
-//    print(result)
-//}
 
-fibo(input)
-print("\(result)\n")
+let count = 1
+if count ~= 11 {
+    print("in")
+} else {
+    print("nin")
+}
